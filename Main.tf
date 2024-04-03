eae6ba0315afcf22c73c4c306d73d90508c5e6c2
SadhuDhanunjaya <sadhu.dhanunjay@diggibyte.com>
2024-04-03 13:49:27 +0530

    removed statefile code

--- Main.tf
+++ Main.tf
@@ -1,10 +1,10 @@
 terraform {
   required_version = ">= 0.12.24"
-  backend "s3" {
-    bucket = "cloudquickpocsbackendtf"
-    key="quickpocsbackendtf.tfstates"
-    region = "us-east-1"
-  }
+#  backend "s3" {
+#    bucket = "cloudquickpocsbackendtf"
+#    key="quickpocsbackendtf.tfstates"
+#    region = "us-east-1"
+#  }
 }
 provider "aws" {
   region = "us-east-1"
