b2f2a2fb54502a135af5b42a7552a758a045f06f
SadhuDhanunjaya <sadhu.dhanunjay@diggibyte.com>
2024-04-03 13:43:05 +0530

    .

---
+++ Main.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_version = ">= 0.12.24"
+  backend "s3" {
+    bucket = "cloudquickpocsbackendtf"
+    key="quickpocsbackendtf.tfstates"
+    region = "us-east-1"
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+}
