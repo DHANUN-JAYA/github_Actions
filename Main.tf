96c93f706559e689e6999df513687fc7125d2eaf
SadhuDhanunjaya <sadhu.dhanunjay@diggibyte.com>
2024-04-04 15:51:29 +0530

    Add module

--- Main.tf
+++ Main.tf
@@ -4,7 +4,7 @@ locals {
 
 
 
-module "storage_account" {
+module "s3" {
   source = "./module/s3"
 
   for_each = { for entry in local.config_data["aws_s3_bucket"] : entry["aws_s3_bucket"] => entry }
