c1d4fde7bf9ace61d71b617da86499bd93dc450a
DHANUN-JAYA <142199082+DHANUN-JAYA@users.noreply.github.com>
2024-04-04 18:12:07 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -5,7 +5,7 @@ locals {
 
 
 module "s3" {
-  source = "./module/s3"
+  source = "../module/s3"
 
   for_each = { for entry in local.config_data["aws_s3_bucket"] : entry["aws_s3_bucket"] => entry }
 
