247b40e3b78cb5a72a1cbfeb270bf6a2208b6ab7
SadhuDhanunjaya <sadhu.dhanunjay@diggibyte.com>
2024-04-05 10:25:23 +0530

    Add module

--- Main.tf
+++ Main.tf
@@ -1,13 +1,13 @@
-locals {
-  config_data = jsondecode(file("${path.module}/config/config.json"))
-}
-
-
-
-module "s3" {
-  source = "../module/s3"
-
-  for_each = { for entry in local.config_data["aws_s3_bucket"] : entry["aws_s3_bucket"] => entry }
-
-  config = each.value
-}
+#locals {
+#  config_data = jsondecode(file("${path.module}/config/config.json"))
+#}
+#
+#
+#
+#module "s3" {
+#  source = "../module/s3"
+#
+#  for_each = { for entry in local.config_data["aws_s3_bucket"] : entry["aws_s3_bucket"] => entry }
+#
+#  config = each.value
+#}
--- providers.tf
+++ providers.tf
@@ -10,3 +10,8 @@ provider "aws" {
 #  region = "us-east-1"
   region="eu-central-1"
 }
+
+resource "aws_s3_bucket" "example" {
+ # bucket = var.config["bucket_name"]
+ bucket = "sadhuu11"
+}
